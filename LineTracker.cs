df5c9f571b0d835a92b1181febd44664b06de7c9
H4kan <szymon.sieradzki184@wp.pl>
2021-10-15 11:01:00 +0200

    further impl

---
+++ LineTracker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RasterPaint
+{
+    public class LineTracker
+    {
+        public LineService LineService { get; set; }
+
+        public Line LastLine { get; set; }
+
+        public Point Origin { get; set; }
+
+        public LineTracker(LineService lineService, int x, int y)
+        {
+            this.Origin = new Point(x, y);
+            this.LineService = lineService;
+            this.LastLine = new Line();
+            this.LastLine.AppendPoint(Origin);
+        }
+
+        public void Update(object sender, MouseEventArgs e)
+        {
+            this.LineService.EraseTrackingLine(LastLine);
+
+            LastLine = this.LineService.CreateTrackingLine(Origin.X, Origin.Y, e.X, e.Y);
+
+            LineService.PictureBox.Invalidate();
+        }
+    }
+}
---
+++ Polygon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace RasterPaint
+{
+    public class Polygon
+    {
+        public List<Line> Edges { get; set; }
+
+        public List<Point> Vertices { get; set; }
+        
+        public Polygon(int x, int y)
+        {
+            Vertices = new List<Point>();
+            Vertices.Add(new Point(x, y));
+            Edges = new List<Line>();
+        }
+         
+        public void AppendLine(Line line)
+        {
+            this.Vertices.Add(line.Points[line.Points.Count - 1]);
+            this.Edges.Add(line);
+        }
+
+        public void CompletePolygon(Line line)
+        {
+            this.Edges.Add(line);
+        }
+    }
+}
